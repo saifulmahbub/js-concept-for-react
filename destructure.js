e44c01127287e5d5f8389cca1d227710f175f6cb
saifulmahbub <saifulislammahbub83@gmail.com>
2024-02-11 13:07:04 +0100

    function destrure

--- destructure.js
+++ destructure.js
@@ -1,5 +1,16 @@
 //Array Destructuring
 const numbers = [20, 12];
-const x = numbers[0];
-const y = numbers[1];
-console.log(x + y);
+// const x = numbers[0];
+// const y = numbers[1];
+//const [x, y] = [20, 12];
+const [x, y] = numbers;
+
+console.log(x, y);
+
+//destructuing function
+const box = (num1, num2) => {
+  const num = [num1, num2];
+  return num;
+};
+const [first, second] = box(2, 4);
+console.log(first);
