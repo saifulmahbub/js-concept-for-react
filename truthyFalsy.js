abf7225a0100246e62901a0c9a96bb93846493d2
saifulmahbub <saifulislammahbub83@gmail.com>
2024-02-12 00:01:16 +0100

    truthy and falsy

---
+++ truthyFalsy.js
@@ -0,0 +1,26 @@
+const myVar = 5;
+if (myVar) {
+  console.log("saiful");
+}
+const name = 0;
+if (!name) {
+  console.log(1);
+} else {
+  console.log(2);
+}
+
+//Ternary Operator
+const money = 101;
+let food;
+// if (money > 100) {
+//   food = "biryani";
+// } else {
+//   food = "cha biscuit";
+// }
+const meal = money > 100 ? (food = "biriyani") : (food = "cha-biscuit");
+// console.log(meal);
+
+//coversion number to string
+const num1 = 52;
+// const str = String(num1);//const str =(num1+ " ");
+console.log(typeof str);
