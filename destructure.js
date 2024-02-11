dfb6c401873c05c3bd7fa309914f9c58d90e8b3c
saifulmahbub <saifulislammahbub83@gmail.com>
2024-02-11 14:37:34 +0100

    destructuring nested object

--- destructure.js
+++ destructure.js
@@ -3,9 +3,9 @@ const numbers = [20, 12];
 // const x = numbers[0];
 // const y = numbers[1];
 //const [x, y] = [20, 12];
-const [x, y] = numbers;
+// const [x, y] = numbers;
 
-console.log(x, y);
+// console.log(x, y);
 
 //destructuing function
 const box = (num1, num2) => {
@@ -13,4 +13,30 @@ const box = (num1, num2) => {
   return num;
 };
 const [first, second] = box(2, 4);
-console.log(first);
+// console.log(first);
+//object destrucring
+
+const Person = {
+  firstName: "saiful",
+  lastName: "islam",
+  age: 28,
+  watch: ["youtube", "facebook", "instagram"],
+};
+const [watch1, watch2, watch3] = Person.watch;
+// console.log(watch2);
+const employee = {
+  ide: "vs code",
+  designation: "developer",
+  machine: "Mac",
+  language: ["html", "css", "react"],
+  specification: {
+    height: 66,
+    weight: 80,
+    address: "noakhali",
+    drink: "water",
+  },
+};
+
+const { machine, ide } = employee;
+const { address, height } = employee.specification;
+console.log(address, height);
