b916216d628b83d92d1d4fefc64434d32af54740
saifulmahbub <saifulislammahbub83@gmail.com>
2024-02-13 12:01:36 +0100

    complete Final

---
+++ notation.js
@@ -0,0 +1,11 @@
+const person = {
+  name: "lal nil holud miya",
+  profession: "traffic surgent",
+  age: 39,
+  address: "shahbag",
+};
+//dot Notation
+console.log(person.name);
+//bracket Notation
+const boyosh = "age";
+console.log(person[boyosh]);
